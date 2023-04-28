10a1b8dfab6977b1388df358e34a209fa8c33b7d
Suraj Ganapati Jadhav <suraj@KLIRnD.COM>
2023-04-28 13:25:53 +0530

    Added EnumClass.cpp

---
+++ Cpp_Tutorials/EnumClass.cpp
@@ -0,0 +1,92 @@
+//Enum class
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+void main()
+{
+    // Defining enum1 Gender
+    enum Gender {
+        Male,
+        Female
+    };
+
+    // Defining enum2 Gender2 with same values
+    // This will throw compilation error
+    /*enum Gender2 {
+        Male,
+        Female
+    };*/
+
+
+    //No variable can have a name which is already in some enumeration:
+    //int Male = 90;    //ERROR
+
+
+    //Enums are not type-safe
+    enum Color {
+        Red,
+        Green
+    };
+
+    Gender gender = Male;
+    Color color = Red;
+
+    // Upon comparing gender and color it will return true as both have value 0
+    // which should not be the case actually
+    if (gender == color)
+        cout << "Equal";
+
+
+    //To solve above issues class enum is introduced
+    enum class Gender3 {
+        Male,
+        Female
+    };
+
+    enum class Color2 {
+        Red,
+        Green
+    };
+
+    //enum class variables can be declared
+    int Green = 90;
+
+    // Instantiating the Enum Class
+    Color2 x = Color2::Green;
+
+    //Comparison now is completely type-safe, following comparison throws compilation error.
+    /*if (x == Color::Red)
+        out << "Equal";*/
+
+    enum class People {
+        Good,
+        Bad
+    };
+
+    People p = People::Good;
+
+    /*if (x == p)
+        cout << "Equal"; */
+
+    
+
+}
+/************************************Summary*****************************************
+1. Enum is a collection of named integer constant means it’s each element is assigned 
+   by integer value.
+
+2. Two enumerations cannot share the same names.
+
+3. No variable can have a name which is already in some enumeration.
+
+4. Enums are not type-safe. Conventional enums implicitly convert to int, causing logical 
+   errors. Comparing two different enums is possible.
+
+5. Class enum doesn’t allow implicit conversion to int, and also doesn’t compare 
+   enumerators from different enumerations. It is both strongly typed and strongly scoped.
+
+6. enum type can be specified while declaration:
+   enum name : type { enumerator = constexpr , enumerator = constexpr , ... }	
+************************************************************************************/
--- Cpp_Tutorials/Resource_Topics.md
+++ Cpp_Tutorials/Resource_Topics.md
@@ -2,9 +2,8 @@
 2. Generalization and specialization.
 3. Multithreading in C++.
 4. Interview questions.
-5. Enum and Enum class.
 6. Move semantics
 7. Template function.
 8. Template class.
-9.  Deadly diamond of death.
+9. Deadly diamond of death.
 10. Default and Delete keyword
