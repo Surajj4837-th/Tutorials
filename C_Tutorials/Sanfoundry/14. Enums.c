2bc43706a735b698139b9bba87c1c156f646cd42
surajj4837-th <surajj4837@gmail.com>
2023-08-21 16:44:34 +0530

    Added output print in comments

--- C_Tutorials/Sanfoundry/14. Enums.c
+++ C_Tutorials/Sanfoundry/14. Enums.c
@@ -1,47 +1,64 @@
-// Enums
+// Array
 #include <stdio.h>
-
-enum day {
-   Sunday = 1,
-   Monday,
-   Tuesday,
-   Wednesday,
-   Thursday,
-   Friday,
-   Saturday
-};
-
-enum denominations{
-   Paisa,
-   Rupee
-};
 
 int main()
 {
+   //*******************************Initialization*****************************//
+
+   char arr1[10] = {'S', 'U', 'R', 'A', 'J'}; // NULL is automatically appended.
+
+   char arr2[] = {'J', 'A', 'D', 'H', 'A', 'V', '\0'};
+
+   char arr3[6];
+   arr3[0] = 'A';
+   arr3[1] = 'r';
+   arr3[2] = 'r';
+   arr3[3] = 'a';
+   arr3[4] = 'y';
+   arr3[5] = '\0';
+
+   //char string
+   char arr4[] = "Suraj";  // NULL is automatically appended.
+   char arr5[] = {"Jadhav"};  // NULL is automatically appended.
+
    //**************************************************************************//
-      
+
+   printf("String length of arr1: %lu\n", strlen(arr1));
+   printf("Size of arr1: %lu\n", sizeof(arr1));
+
+   //**************************************************************************//
+
+   strcpy(arr2, arr1);
+   printf("%s\n", arr1);
+   printf("%s\n", arr2);
+
+   //**************************************************************************//
+   int arr6[9] = {};       //Bracket initialization assigns uninitialized values to 0
+   int arr7[9] = {1,2,3};  //Bracket initialization assigns uninitialized values to 0
+   int arr8[9];
+
+   printf("arr6[4]: %d\n", arr6[4]);
+   printf("arr7[4]: %d\n", arr7[4]);
+   printf("arr8[4]: %d\n", arr8[4]);
+
    //**************************************************************************//
     
 }
 
 /*************************************OUTPUT***************************************
-
+String length of arr1: 5
+Size of arr1: 10
+SURAJ
+SURAJ
+arr6[4]: 0
+arr7[4]: 0
+arr8[4]: -9690
 ***********************************************************************************/
 
 /************************************Summary****************************************
-1. Enum is a named integer constant.
+1. Array: collection of same data type items.
 
-2. It helps is code readability and maintainability.
+2. Data storage is contiguous.
 
-3. The value assigned to 1st enum member is 0 by default. Next value is always previous + 1.
-
-4. Enum vs MACRO
-   - MACRO is a preprocessor directive that is replaced with the value before compile time.
-   - The type of enum in C is an integer, but the type of macro can be of any type.
-   - In Enum, if we do not assign the values to the enum names, then the compiler will 
-      automatically assign the default value to the enum names. But, in the case of 
-      macro, the values need to be explicitly assigned.
-   - The enum type follows the scope rules while macro does not follow the scope rules.
-   - In terms of readability, enumerations make better constants than macros, because 
-      related values are grouped together.
+3. {} initialization assigns 0 to uninitialized places.
 ************************************************************************************/
