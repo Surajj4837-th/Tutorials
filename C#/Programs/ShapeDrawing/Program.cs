10f18895efc9a89107986543590980ea5f8868d1
Surajj4837-th <surajj4837@gmail.com>
2024-03-02 21:09:04 +0530

    Added shape drawing project.

---
+++ C#/Programs/ShapeDrawing/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace DrawingAShape
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");     //Write this text on the console.
+        }
+    }
+}
+/*
+ * Notes
+ * 1. A class is a container, inside a class we can write all the code for a program for execution.
+ * 
+ * 2. static void Main(string[] args) is a method or a function. It is an important method. The required 
+ *    program needs to be ut inside this method.
+ *    
+ * 3. 
+ */
---
+++ C#/ReadMe.md
@@ -0,0 +1,6 @@
+# C# Tutorial
+
+Source: [C# Tutorial - Full Course for Beginners by freeCodeCamp.org](https://youtu.be/GhQdlIFylQ8?si=c27hZdPRVMAfmGoU)
+
+## Lecture Sequence: 
+1. 
