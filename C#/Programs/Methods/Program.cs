8864e8b56738b3ab7dac98c9d274fa691e9ff3cf
Surajj4837-th <surajj4837@gmail.com>
2024-03-03 19:51:54 +0530

    Adding methods/funciton code.

---
+++ C#/Programs/Methods/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Print("Suraj");
+            Print("Ashish");
+        }
+
+        static void Print(string name)
+        {
+            Console.WriteLine("Hello " + name + "!");
+        }
+    }
+}
