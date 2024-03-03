df67c46af859aeab682ef8d9c2867914b95ac57b
Surajj4837-th <surajj4837@gmail.com>
2024-03-03 20:38:50 +0530

    Added better calculator code.

---
+++ C#/Programs/BetterCalculator/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace BetterCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter a number: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            char oper;
+            Console.WriteLine("Enter an operator (+,-,*,/): ");
+            oper = Convert.ToChar(Console.Read());
+
+            if(oper == '+')
+            {
+                Console.WriteLine(num1 + num2);
+            }
+            else if (oper == '-')
+            {
+                Console.WriteLine(num1 - num2);
+            }
+            else if (oper == '/')
+            {
+                Console.WriteLine(num1 / num2);
+            }
+            else if (oper == '*')
+            {
+                Console.WriteLine(num1 * num2);
+            }
+            else 
+            {
+                Console.WriteLine("Invalid operator");
+            }
+        }
+    }
+}
