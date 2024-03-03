14a8bf2b34a674f279fc9b072a78afee4df52e3a
Surajj4837-th <surajj4837@gmail.com>
2024-03-03 20:01:46 +0530

    Added code for return statement.

---
+++ C#/Programs/Return/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Return
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Print("Suraj"));
+            Console.WriteLine(Print("Ashish"));
+        }
+
+        static int Print(string name)
+        {
+            Console.WriteLine("Hello " + name + "!");
+
+            return (name.Length);
+        }
+    }
+}
