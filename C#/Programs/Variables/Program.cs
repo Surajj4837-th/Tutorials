c9c26f2c18a7a48a92953011547e503b2afafe74
Surajj4837-th <surajj4837@gmail.com>
2024-03-03 09:19:11 +0530

    Adding variables tutorial code.

---
+++ C#/Programs/Variables/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Suraj";
+            int age = 30;
+            
+            Console.WriteLine("Hello " + name + "!");
+            Console.WriteLine("You are of " + age + " years.");
+
+            //modify variables
+            name = "Ashish";
+            Console.WriteLine("Hello " + name + "!");
+        }
+    }
+}
+/*
+ * 1. Variables are containers for storing data values. 
+ * 
+ * 2. Values can be string, numbers, etc.
+ */
