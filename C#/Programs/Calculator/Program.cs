89fbeefbdcb7b6ffb3ea2ec7604a03fd12f1fae2
Surajj4837-th <surajj4837@gmail.com>
2024-03-03 17:59:36 +0530

    Adding calculator code.

---
+++ C#/Programs/Calculator/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Transactions;
+
+namespace Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = Convert.ToInt32("45");    //convert string to integer
+
+            Console.WriteLine("Enter a number: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter another number: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine(num1 + num2);
+
+        }
+    }
+}
+/*
+ * 1. If there is an input of decimal and the convert menthod selected is of integer then
+ *    there is an runtime error. So to work with decimal numbers we use double and convert the
+ *    input to the same.
+ */
