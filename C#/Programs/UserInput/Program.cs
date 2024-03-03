4d1d89a60ca1685f3926dda757b472c339110aff
Surajj4837-th <surajj4837@gmail.com>
2024-03-03 17:13:46 +0530

    Adding user input code.

---
+++ C#/Programs/UserInput/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace UserInput
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");     // Print output and go to next line
+            Console.Write("Hello");                 // Print output and remain on same line
+
+            Console.Write("\nEnter your name: ");
+
+            string name = Console.ReadLine();       // Get input from console
+
+            Console.WriteLine("Hello " + name);
+
+            Console.WriteLine("Enter your age: ");
+
+            string age = Console.ReadLine();
+
+            Console.WriteLine("Your age: " + age);
+        }
+    }
+}
