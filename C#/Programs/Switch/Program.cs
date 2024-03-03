75d73d7a9e5ad797a78608ad51aefe290de4f43f
Surajj4837-th <surajj4837@gmail.com>
2024-03-03 20:46:43 +0530

    Added better calculator code.

---
+++ C#/Programs/Switch/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace Switch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetDay(0));
+        }
+
+        static string GetDay(int dayNum)
+        {
+            string dayName;
+            switch(dayNum)
+            {
+                case 0:
+                    dayName = "Sunday";
+                    break;
+
+                case 1:
+                    dayName = "Monday";
+                    break;
+
+                case 2:
+                    dayName = "Tuesday";
+                    break;
+
+                case 3:
+                    dayName = "Wednesday";
+                    break;
+
+                case 4:
+                    dayName = "Thursday";
+                    break;
+
+                case 5:
+                    dayName = "Friday";
+                    break;
+
+                case 6:
+                    dayName = "Saturday";
+                    break;
+
+                default:
+                    dayName = "Invalid day number";
+                    break;
+            }
+
+            return dayName;
+        }
+    }
+}
