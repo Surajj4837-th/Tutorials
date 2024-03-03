5ac896c7341f240ec63d04d1dc2da9ac6702f829
Surajj4837-th <surajj4837@gmail.com>
2024-03-03 20:51:27 +0530

    Added while loop code.

---
+++ C#/Programs/WhileLoop/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace WhileLoop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int idx = 0;
+
+            while(idx <= 5)         // first check this condition and then enter in the loop
+            {
+                Console.WriteLine("Hello, World! " + idx++);        //increment index
+            }
+
+            do          //first execute the loop and then check the condition
+            {
+                Console.WriteLine("Hello, World! " + idx++);
+
+            } while (idx <= 5);
+        }
+    }
+}
