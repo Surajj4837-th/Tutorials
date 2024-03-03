13f1bc95411a955e52ba87fa45e9bb05930b8a59
Surajj4837-th <surajj4837@gmail.com>
2024-03-03 09:51:11 +0530

    Adding data types code.

---
+++ C#/Programs/DataTypes/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace DataTypes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Text data types
+            string str = "Tutorial";
+            char ch = 'a';
+
+            //Numeric types
+            int num1 = 10;
+            int num2 = -10;
+            float fl = 9.9F;
+            double db = 0.8;
+            decimal dc = 3.44M;
+
+            //Boolean
+            bool isSunday = true;
+
+            //constant
+            Console.WriteLine("400");
+        }
+    }
+}
+/*
+ * 1. There are 3 decimal number data types: float, double and decimal. float having least precicion
+ *    and decimal having most precision.
+ *    
+ * 2. F is appended for a float variable, M is appended for a decimal variable.
+ */
