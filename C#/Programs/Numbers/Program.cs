dda3d16425739a4cbd22fef7d17f8509e0080b12
Surajj4837-th <surajj4837@gmail.com>
2024-03-03 17:03:40 +0530

    Added numbers code.

---
+++ C#/Programs/Numbers/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(1 + 2);
+            Console.WriteLine(1 / 2);
+            Console.WriteLine(1 / 2.0);     // answer in decimal
+            Console.WriteLine(1 * 2);
+            Console.WriteLine(1 % 2);     //remainder
+
+            //Short hand notations
+            int num = 3;
+            Console.WriteLine(num++);
+            Console.WriteLine(++num);
+
+            // Math methods
+            Console.WriteLine(Math.Abs(-40));
+            Console.WriteLine(Math.Pow(3, 2));
+            
+        }
+    }
+}
+// Explore math methods for more funcitons.
