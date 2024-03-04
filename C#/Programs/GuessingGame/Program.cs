329d9711f9f26a9e3ae9902d67ce7e76405f5cc9
Surajj4837-th <surajj4837@gmail.com>
2024-03-04 21:23:44 +0530

    Guessing game code added.

---
+++ C#/Programs/GuessingGame/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace GuessingGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Guess the secret word within 3 times.
+
+            string secretWord = "CSharp";
+            string guess = "";
+
+            int count = 0;
+            int guessLimit = 3;
+            bool outOfGuesses = false;
+
+            while(guess != secretWord && !outOfGuesses)
+            {
+                if (count < guessLimit)
+                {
+                    Console.WriteLine("Enter guess: ");
+                    guess = Console.ReadLine();
+                    count++;
+                }
+                else
+                {
+                    outOfGuesses = true;
+                }
+            }
+
+            if(outOfGuesses)
+            {
+                Console.WriteLine("You lose!");
+            }
+            else
+            {
+                Console.WriteLine("You win!");
+            }
+        }
+    }
+}
