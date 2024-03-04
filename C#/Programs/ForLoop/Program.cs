ea53067c0def02b26c43e4d83ae271d25766e3f3
Surajj4837-th <surajj4837@gmail.com>
2024-03-04 21:45:51 +0530

    Added for loop code.

---
+++ C#/Programs/ForLoop/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ForLoop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            while(i <= 5)
+            {
+                Console.WriteLine("Hello, World!");
+                i++;
+            }
+
+            for(int j = 0; j < 5 ; j++)
+            {
+                Console.WriteLine("Hello, World!");
+            }
+        }
+    }
+}
