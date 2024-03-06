44c06ba601d60fa27f601d9b4239b2f33c1f3e88
Surajj4837-th <surajj4837@gmail.com>
2024-03-06 20:06:16 +0530

    Added exponent code.

---
+++ C#/Programs/Exponent/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Exponent
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetPow(3,3));
+        }
+
+        static int GetPow(int basenum, int pownum)
+        {
+            int result = 1;
+
+            for (int i = 0; i < pownum; i++)
+            {
+                result *= basenum;
+            }
+
+            return result;
+        }
+    }
+}
