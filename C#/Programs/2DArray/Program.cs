75989ba3b939230e7ab29cd3e0b6410f524a6edd
Surajj4837-th <surajj4837@gmail.com>
2024-03-06 20:25:17 +0530

    Added 2D array code.

---
+++ C#/Programs/2DArray/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _2DArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] arr2d =      //Array initialization when values are known
+            {
+                {1,2},
+                {3,4},
+                {5,6}
+            };
+
+            int[,] arr = new int[2, 3]; // array initialization with values unknown
+                                        // but size known, 2 rows, 3 columns
+
+            Console.WriteLine(arr2d[0,1]);
+        }
+    }
+}
