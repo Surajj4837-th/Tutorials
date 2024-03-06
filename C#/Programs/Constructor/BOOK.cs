292e5289f85a0991f71ea0c72f2050c6f1f925d0
Surajj4837-th <surajj4837@gmail.com>
2024-03-07 05:29:59 +0530

    Added constructor code.

---
+++ C#/Programs/Constructor/BOOK.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor
+{
+    internal class BOOK
+    {
+        public string title;
+        public string author;
+        public int pages;
+
+        public BOOK(string atitle, string aauthor, int apages)
+        {
+            title = atitle;
+            author = aauthor;
+            pages = apages;
+        }
+
+        public BOOK()
+        {
+
+        }
+    }
+}
---
+++ C#/Programs/Constructor/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Constructor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BOOK book1 = new BOOK("Maths", "RD Sharma", 1040);
+
+            Console.WriteLine(book1.title);
+            Console.WriteLine(book1.author);
+            Console.WriteLine(book1.pages);
+
+            BOOK book2 = new BOOK("Science", "NCERT", 100);
+
+            Console.WriteLine(book2.title);
+            Console.WriteLine(book2.author);
+            Console.WriteLine(book2.pages);
+
+            BOOK book3 = new BOOK();
+        }
+    }
+}
