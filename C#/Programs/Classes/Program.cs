873f7693125397de3bec82c7a35bbb4510662e4f
Surajj4837-th <surajj4837@gmail.com>
2024-03-06 21:25:48 +0530

    Added class code.

---
+++ C#/Programs/Classes/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BOOK  book1 = new BOOK();
+
+            book1.title = "Maths";
+            book1.author = "RD Sharma";
+            book1.pages = 1040;
+
+            Console.WriteLine(book1.title);
+            Console.WriteLine(book1.author);
+            Console.WriteLine(book1.pages);
+
+            BOOK book2 = new BOOK();
+
+            book2.title = "Science";
+            book2.author = "NCERT";
+            book2.pages = 100;
+
+            Console.WriteLine(book2.title);
+            Console.WriteLine(book2.author);
+            Console.WriteLine(book2.pages);
+        }
+    }
+}
