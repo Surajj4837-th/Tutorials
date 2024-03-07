21e0b72416b157ef35b1454cbd568ef9c9431005
Surajj4837-th <surajj4837@gmail.com>
2024-03-07 05:37:22 +0530

    Added class methods code.

---
+++ C#/Programs/ObjectMethods/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ObjectMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            STUDENT stu1 = new STUDENT("A", "Business", 2.8);
+            STUDENT stu2 = new STUDENT("B", "Business", 3.6);
+
+            Console.WriteLine(stu1.HasHonours());
+            Console.WriteLine(stu2.HasHonours());
+        }
+    }
+}
---
+++ C#/Programs/ObjectMethods/STUDENT.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectMethods
+{
+    internal class STUDENT
+    {
+        public string name;
+        public string major;
+        public double gpa;
+        public STUDENT(string aname, string amajor, double agpa) 
+        {
+            name = aname;
+            major = amajor;
+            gpa = agpa;
+        }
+
+        public bool HasHonours()
+        {
+            if(gpa >= 3.5)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
