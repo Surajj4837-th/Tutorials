df5130b34d51a73a2eb3b60292b6e9d6ed1e7b29
Surajj4837-th <surajj4837@gmail.com>
2024-03-08 06:29:04 +0530

    Added setter and getter code.

---
+++ C#/Programs/GetterSetter/MOVIE.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetterSetter
+{
+    internal class MOVIE
+    {
+        private string name;
+        private string director;
+        private string rating;   // P, PG, PG-13, R, NR
+
+        public MOVIE(string aname, string adirector, string arating)
+        {
+            name = aname;
+            director = adirector;
+            Rating= arating;        //Use setter function instead of direct assignment
+        }
+
+        public string Rating
+        {
+            get
+            {
+                return rating;
+
+            }
+            set
+            {
+                if(value == "G" || value == "PG" || value == "PG-13" || value == "R" || value == "NR")
+                {
+                    rating = value;
+                }
+                else
+                {
+                    rating = "NR";
+                }
+            }
+        }
+    }
+}
---
+++ C#/Programs/GetterSetter/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace GetterSetter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MOVIE avengers = new MOVIE("The Avengers", "Joss", "Dog");
+            MOVIE shrek = new MOVIE("Shrek", "Adam", "PG");
+
+            shrek.Rating = "Cat";
+
+            Console.WriteLine(avengers.Rating);
+            Console.WriteLine(shrek.Rating);
+        }
+    }
+}
+
+/*
+ * 1. Set and get are C# provided functions for each class attributes.
+ * 
+ * 2. Set and get are declared inside a block in class.
+ * 
+ * 3. It is better to call set() in constructor for checking valid input.
+ */
