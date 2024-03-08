975990e221d183cdefe578dc1231f1cf01b5f82b
Surajj4837-th <surajj4837@gmail.com>
2024-03-08 06:52:00 +0530

    Added static class attributes code.

---
+++ C#/Programs/StaticClassAttributes/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace StaticClassAttributes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SONG holiday = new SONG("Holiday", "Green Day", 200);
+            Console.WriteLine(SONG.songCount);
+            SONG Kashmir = new SONG("Kashmir", "Led Zeppelin", 150);
+            Console.WriteLine(SONG.songCount);
+
+            //Console.WriteLine(Kashmir.songCount);     //error
+        }
+    }
+}
+/*
+ * 1. Static attributes are special attributes which are shared by all objects of the class.
+ * 
+ * 2. static attributes can be accessed using class and not by using any class object.
+ * 
+ * 3. To access a static attribute using object we can use a method which returns the attribute.
+ */
---
+++ C#/Programs/StaticClassAttributes/SONG.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticClassAttributes
+{
+    internal class SONG
+    {
+        public string title;
+        public string artist;
+        public int duration;
+        public static int songCount = 0;
+
+        public SONG(string atitle, string aartist, int aduration)
+        {
+            title = atitle;
+            artist = aartist;
+            duration = aduration;
+            songCount++;
+        }
+    }
+}
