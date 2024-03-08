8b6767b9590bb1c3721554f65132d49a9950c639
Surajj4837-th <surajj4837@gmail.com>
2024-03-08 16:48:07 +0530

    Added code for tutorial 2: Created form page. Created 3 buttons. Assigned task on button clicks. Created label.

---
+++ C#/Windows_Form_Application/VPApp1/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace VPApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblHello = new System.Windows.Forms.Label();
+            this.btnset = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.lgtot = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblHello
+            // 
+            this.lblHello.AutoSize = true;
+            this.lblHello.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHello.Location = new System.Drawing.Point(40, 40);
+            this.lblHello.Name = "lblHello";
+            this.lblHello.Size = new System.Drawing.Size(86, 31);
+            this.lblHello.TabIndex = 0;
+            this.lblHello.Text = "label1";
+            this.lblHello.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // btnset
+            // 
+            this.btnset.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnset.Location = new System.Drawing.Point(33, 332);
+            this.btnset.Name = "btnset";
+            this.btnset.Size = new System.Drawing.Size(100, 49);
+            this.btnset.TabIndex = 1;
+            this.btnset.Text = "Set";
+            this.btnset.UseVisualStyleBackColor = true;
+            this.btnset.Click += new System.EventHandler(this.btnset_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancel.Location = new System.Drawing.Point(168, 332);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(100, 49);
+            this.btnCancel.TabIndex = 2;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // lgtot
+            // 
+            this.lgtot.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lgtot.Location = new System.Drawing.Point(642, 332);
+            this.lgtot.Name = "lgtot";
+            this.lgtot.Size = new System.Drawing.Size(100, 49);
+            this.lgtot.TabIndex = 3;
+            this.lgtot.Text = "Logout";
+            this.lgtot.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lgtot);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnset);
+            this.Controls.Add(this.lblHello);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblHello;
+        private System.Windows.Forms.Button btnset;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button lgtot;
+    }
+}
+
---
+++ C#/Windows_Form_Application/VPApp1/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VPApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblHello.Text = "Hello World";
+        }
+
+        private void btnset_Click(object sender, EventArgs e)
+        {
+            lblHello.Text = "Hello World Again";
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            lblHello.Text = "Cancelled";
+        }
+    }
+}
+
+/*
+ * 1. Form1 is the complete form.
+ * 
+ * 2. Form1_Load() is function that will do operatins when form is application is loaded
+ */
