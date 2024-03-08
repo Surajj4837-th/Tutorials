78213cfeb716f8704d0d0b9e6e0e0aa44f64c5bf
Surajj4837-th <surajj4837@gmail.com>
2024-03-08 10:31:02 +0530

    Added code for static methods and class.

---
+++ C#/Programs/StaticMethodsClasses/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace StaticMethodsClasses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            USEFUL_TOOL.SayHi("Suraj");
+
+            USEFUL_TOOL tool = new USEFUL_TOOL();
+
+            //tool.SayHi("Suraj"); //error
+
+            Console.WriteLine(Math.Sqrt(144));      //static class: Math
+
+            //Math myMath = new Math();       //illegal
+
+        }
+    }
+}
+/*
+ * 1. static method is the method that belongs to the class itself.
+ * 
+ * 2. Objects cannot call static methods.
+ * 
+ * 3. Similarly there are static classes. These can be used directly and objects cannot be created 
+ *    from it. Example is Math class in C#, 
+ *    
+ * 4. Declaring a class as static will create a static class.
+ */
---
+++ C#/Programs/StaticMethodsClasses/USEFUL_TOOL.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticMethodsClasses
+{
+    internal /*static*/ class USEFUL_TOOL       //Add static keyword to make class as static
+    {
+        public static void SayHi(string name)
+        {
+            Console.WriteLine("Hi " + name);
+        }
+    }
+}
