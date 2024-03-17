399f320eb380ea950d9e871dca7360fc45729dab
Surajj4837-th <surajj4837@gmail.com>
2024-03-17 18:35:08 +0530

    Added code to write user data to SQL table using SQL query. Secured user data.

--- C#/Windows_Form_Application/DatabaseProject/SignUp.Designer.cs
+++ C#/Windows_Form_Application/DatabaseProject/SignUp.Designer.cs
@@ -44,9 +44,10 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(342, 31);
+            this.label1.Location = new System.Drawing.Point(256, 25);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(146, 39);
+            this.label1.Size = new System.Drawing.Size(117, 31);
             this.label1.TabIndex = 0;
             this.label1.Text = "Sign Up";
             // 
@@ -54,53 +55,59 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(86, 103);
+            this.label2.Location = new System.Drawing.Point(64, 84);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(98, 36);
+            this.label2.Size = new System.Drawing.Size(61, 24);
             this.label2.TabIndex = 1;
             this.label2.Text = "Name";
             // 
             // textBox1
             // 
             this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(282, 100);
+            this.textBox1.Location = new System.Drawing.Point(212, 81);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(417, 34);
+            this.textBox1.Size = new System.Drawing.Size(314, 28);
             this.textBox1.TabIndex = 2;
             // 
             // textBox2
             // 
             this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox2.Location = new System.Drawing.Point(282, 160);
+            this.textBox2.Location = new System.Drawing.Point(212, 130);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(417, 34);
+            this.textBox2.Size = new System.Drawing.Size(314, 28);
             this.textBox2.TabIndex = 4;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(86, 163);
+            this.label3.Location = new System.Drawing.Point(64, 132);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(103, 36);
+            this.label3.Size = new System.Drawing.Size(63, 24);
             this.label3.TabIndex = 3;
             this.label3.Text = "E-Mail";
             // 
             // textBox3
             // 
             this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox3.Location = new System.Drawing.Point(282, 212);
+            this.textBox3.Location = new System.Drawing.Point(212, 172);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(417, 34);
+            this.textBox3.Size = new System.Drawing.Size(314, 28);
             this.textBox3.TabIndex = 6;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(86, 215);
+            this.label4.Location = new System.Drawing.Point(64, 175);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(150, 36);
+            this.label4.Size = new System.Drawing.Size(92, 24);
             this.label4.TabIndex = 5;
             this.label4.Text = "Password";
             // 
@@ -108,36 +115,46 @@
             // 
             this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(282, 283);
+            this.comboBox1.Items.AddRange(new object[] {
+            "India",
+            "UK",
+            "USA",
+            "South Korea",
+            "France"});
+            this.comboBox1.Location = new System.Drawing.Point(212, 230);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(417, 37);
+            this.comboBox1.Size = new System.Drawing.Size(314, 30);
             this.comboBox1.TabIndex = 7;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(86, 284);
+            this.label5.Location = new System.Drawing.Point(64, 231);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(119, 36);
+            this.label5.Size = new System.Drawing.Size(75, 24);
             this.label5.TabIndex = 8;
             this.label5.Text = "Country";
             // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(326, 349);
+            this.button1.Location = new System.Drawing.Point(244, 284);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(186, 71);
+            this.button1.Size = new System.Drawing.Size(140, 58);
             this.button1.TabIndex = 9;
             this.button1.Text = "Sign Up";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // SignUp
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(600, 366);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.comboBox1);
@@ -148,6 +165,7 @@
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "SignUp";
             this.Text = "SignUp";
             this.ResumeLayout(false);
--- C#/Windows_Form_Application/DatabaseProject/SignUp.cs
+++ C#/Windows_Form_Application/DatabaseProject/SignUp.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,10 +13,63 @@ namespace DatabaseProject
 {
     public partial class SignUp : Form
     {
+        DBAccess DBObject = new DBAccess();
         public SignUp()
         {
             InitializeComponent();
         }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string username = textBox1.Text;
+            string useremail = textBox2.Text;
+            string userpassword = textBox3.Text;
+            string usercountry = comboBox1.Text;
+
+            if(username.Equals(""))
+            {
+                MessageBox.Show("Please enter your user name.");
+            }
+            else if (useremail.Equals(""))
+            {
+                MessageBox.Show("Please enter your user e-mail.");
+            }
+            else if (userpassword.Equals(""))
+            {
+                MessageBox.Show("Please enter your user password.");
+            }
+            else if (usercountry.Equals(""))
+            {
+                MessageBox.Show("Please enter your user country.");
+            }
+            else
+            {
+                SqlCommand insertCommand = new SqlCommand("insert into Users(Name,Email,Password,Country) values(@username, @useremail, @userpassword, @usercountry)");
+
+                //Securing the data: by using following code the user data is hidden to DBObject
+                insertCommand.Parameters.AddWithValue("@username", username);
+                insertCommand.Parameters.AddWithValue("@useremail", useremail);
+                insertCommand.Parameters.AddWithValue("@userpassword", userpassword);
+                insertCommand.Parameters.AddWithValue("@usercountry", usercountry);
+
+                int row = DBObject.executeQuery(insertCommand);
+
+                if (row == 1)
+                {
+                    MessageBox.Show("Account is created successfully...");
+
+                    this.Hide();
+
+                    HomePage home = new HomePage();
+
+                    home.Show();
+                }
+                else
+                {
+                    MessageBox.Show("Error occured.");
+                }
+            }
+            
+        }
     }
 }
