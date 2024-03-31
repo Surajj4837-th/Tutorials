4cc21d824fba9dadc3a32d2f17144e5b349d34b7
Surajj4837-th <surajj4837@gmail.com>
2024-03-31 21:33:01 +0530

    Added code to insert records.

---
+++ SQL/CodeSnippets/InsertRecord.sql
@@ -0,0 +1,23 @@
+insert into employee values (
+	1, 'Suraj', 50000, 30, 'M', 'ADAS'
+);
+
+insert into employee values (
+	2, 'Ashish', 500000, 28, 'M', 'HR'
+);
+
+insert into employee values (
+	3, 'Rupali', 100000, 25, 'F', 'Analytics'
+);
+
+insert into employee values (
+	4, 'Ramesh', 80000, 23, 'M', 'Admin'
+);
+
+insert into employee values (
+	5, 'Suresh', 180000, 31, 'M', 'Admin'
+);
+
+insert into employee values (
+	6, 'Deepali', 100000, 25, 'F', 'HR'
+);
