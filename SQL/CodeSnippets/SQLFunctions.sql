72951a79447d6612cc7ebae4bcaf659976bab52d
Surajj4837-th <surajj4837@gmail.com>
2024-03-31 21:33:59 +0530

    Added code to use SQL functions,

---
+++ SQL/CodeSnippets/SQLFunctions.sql
@@ -0,0 +1,9 @@
+select min(salary) from employee;
+
+select max(age) from employee;
+
+select count(*) from employee where gender='M';
+
+select sum(salary) from employee;
+
+select avg(age) from employee;
