3099ba8d297cfbe99775a3595bedb3a8f7ec19c9
Surajj4837-th <surajj4837@gmail.com>
2024-03-31 21:34:33 +0530

    Added code to use string functions.

---
+++ SQL/CodeSnippets/StringFunctions.sql
@@ -0,0 +1,11 @@
+select '         spaces'
+
+select ltrim('    spaces')
+
+select 'I M SURAJ'
+select lower('I M SURAJ')
+select upper('i m suraj')
+
+select reverse('i m suraj')
+
+select substring('i m suraj', 5, 5)
