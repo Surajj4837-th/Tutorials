d156a3071fb212c50c4199ef2bf8d36e35d8971b
Surajj4837-th <surajj4837@gmail.com>
2024-04-02 21:28:30 +0530

    Added code to delete records from table.

---
+++ SQL/CodeSnippets/Delete.sql
@@ -0,0 +1,3 @@
+delete from employee where name='Mahesh';	--No rows will be affected as this record does not exist.
+delete from employee where name='Ramesh';	--Row/record deleted
+select * from employee;
