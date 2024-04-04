f4f640bfaa6731757efb934ab39d37e293c3198a
Surajj4837-th <surajj4837@gmail.com>
2024-04-04 21:27:50 +0530

    Added code to use alter table.

--- SQL/CodeSnippets/AlterTable.sql
+++ SQL/CodeSnippets/AlterTable.sql
@@ -1,3 +1,13 @@
 use happy;
 
 select * from employee;
+
+alter table employee
+Add JoiningDate date;
+
+select * from employee;
+
+alter table employee
+drop column JoiningDate;
+
+select * from employee;
