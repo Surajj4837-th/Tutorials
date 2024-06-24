9d134143bed78c708ac58f434cdde874be466930
Surajj4837-th <surajj4837@gmail.com>
2024-06-24 21:25:07 +0530

    Added controls for accepting user input. Added code to compute total amount. Added logo. Added handlers to close application, print preview.

---
+++ C#/Windows_Form_Application/PDFCreator/MainForm.Designer.cs
@@ -0,0 +1,313 @@
+﻿namespace PDFCreator
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.name = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.mobno = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.productlist = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.quantitycb = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.price = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.totalamount = new System.Windows.Forms.TextBox();
+            this.addcustomer = new System.Windows.Forms.Button();
+            this.print = new System.Windows.Forms.Button();
+            this.preview = new System.Windows.Forms.Button();
+            this.close = new System.Windows.Forms.Button();
+            this.printDocument = new System.Drawing.Printing.PrintDocument();
+            this.printPreviewDialog = new System.Windows.Forms.PrintPreviewDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(439, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(284, 61);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(24, 156);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(167, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Customer Name";
+            // 
+            // name
+            // 
+            this.name.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.name.Location = new System.Drawing.Point(389, 147);
+            this.name.Name = "name";
+            this.name.Size = new System.Drawing.Size(394, 30);
+            this.name.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(24, 236);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(126, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Mobile Num";
+            // 
+            // mobno
+            // 
+            this.mobno.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mobno.Location = new System.Drawing.Point(389, 237);
+            this.mobno.Name = "mobno";
+            this.mobno.Size = new System.Drawing.Size(394, 30);
+            this.mobno.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(24, 331);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 25);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Product";
+            // 
+            // productlist
+            // 
+            this.productlist.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.productlist.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.productlist.FormattingEnabled = true;
+            this.productlist.Items.AddRange(new object[] {
+            "Rasgulla",
+            "Rabdi",
+            "Ladoo",
+            "Barfi",
+            "Rasmalai",
+            "Peda",
+            "Modak",
+            "Halwa",
+            "Soan Papdi",
+            "Gulab Jamun",
+            "Jalebi",
+            "Samosa"});
+            this.productlist.Location = new System.Drawing.Point(389, 321);
+            this.productlist.Name = "productlist";
+            this.productlist.Size = new System.Drawing.Size(394, 33);
+            this.productlist.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(24, 401);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(93, 25);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Quantity";
+            // 
+            // quantitycb
+            // 
+            this.quantitycb.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.quantitycb.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.quantitycb.FormattingEnabled = true;
+            this.quantitycb.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "10",
+            "20",
+            "50",
+            "100"});
+            this.quantitycb.Location = new System.Drawing.Point(389, 397);
+            this.quantitycb.Name = "quantitycb";
+            this.quantitycb.Size = new System.Drawing.Size(394, 33);
+            this.quantitycb.TabIndex = 3;
+            this.quantitycb.TextChanged += new System.EventHandler(this.quantitycb_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(24, 489);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(61, 25);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Price";
+            // 
+            // price
+            // 
+            this.price.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.price.Location = new System.Drawing.Point(389, 482);
+            this.price.Name = "price";
+            this.price.Size = new System.Drawing.Size(394, 30);
+            this.price.TabIndex = 2;
+            this.price.TextChanged += new System.EventHandler(this.price_TextChanged);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(24, 572);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(141, 25);
+            this.label7.TabIndex = 1;
+            this.label7.Text = "Total Amount";
+            // 
+            // totalamount
+            // 
+            this.totalamount.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.totalamount.Location = new System.Drawing.Point(389, 566);
+            this.totalamount.Name = "totalamount";
+            this.totalamount.ReadOnly = true;
+            this.totalamount.Size = new System.Drawing.Size(394, 30);
+            this.totalamount.TabIndex = 2;
+            // 
+            // addcustomer
+            // 
+            this.addcustomer.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addcustomer.Location = new System.Drawing.Point(890, 142);
+            this.addcustomer.Name = "addcustomer";
+            this.addcustomer.Size = new System.Drawing.Size(229, 53);
+            this.addcustomer.TabIndex = 4;
+            this.addcustomer.Text = "Add New Customer";
+            this.addcustomer.UseVisualStyleBackColor = true;
+            this.addcustomer.Click += new System.EventHandler(this.addcustomer_Click);
+            // 
+            // print
+            // 
+            this.print.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.print.Location = new System.Drawing.Point(890, 277);
+            this.print.Name = "print";
+            this.print.Size = new System.Drawing.Size(229, 53);
+            this.print.TabIndex = 4;
+            this.print.Text = "Print";
+            this.print.UseVisualStyleBackColor = true;
+            // 
+            // preview
+            // 
+            this.preview.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.preview.Location = new System.Drawing.Point(890, 411);
+            this.preview.Name = "preview";
+            this.preview.Size = new System.Drawing.Size(229, 53);
+            this.preview.TabIndex = 4;
+            this.preview.Text = "Print Preview";
+            this.preview.UseVisualStyleBackColor = true;
+            this.preview.Click += new System.EventHandler(this.preview_Click);
+            // 
+            // close
+            // 
+            this.close.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.close.Location = new System.Drawing.Point(890, 544);
+            this.close.Name = "close";
+            this.close.Size = new System.Drawing.Size(229, 53);
+            this.close.TabIndex = 4;
+            this.close.Text = "Close";
+            this.close.UseVisualStyleBackColor = true;
+            this.close.Click += new System.EventHandler(this.close_Click);
+            // 
+            // printDocument
+            // 
+            this.printDocument.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(this.printDocument_PrintPage);
+            // 
+            // printPreviewDialog
+            // 
+            this.printPreviewDialog.AutoScrollMargin = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog.AutoScrollMinSize = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog.ClientSize = new System.Drawing.Size(400, 300);
+            this.printPreviewDialog.Enabled = true;
+            this.printPreviewDialog.Icon = ((System.Drawing.Icon)(resources.GetObject("printPreviewDialog.Icon")));
+            this.printPreviewDialog.Name = "printPreviewDialog";
+            this.printPreviewDialog.Visible = false;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1176, 773);
+            this.Controls.Add(this.close);
+            this.Controls.Add(this.preview);
+            this.Controls.Add(this.print);
+            this.Controls.Add(this.addcustomer);
+            this.Controls.Add(this.quantitycb);
+            this.Controls.Add(this.productlist);
+            this.Controls.Add(this.totalamount);
+            this.Controls.Add(this.price);
+            this.Controls.Add(this.mobno);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.name);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Shop";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox name;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox mobno;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox productlist;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox quantitycb;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox price;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox totalamount;
+        private System.Windows.Forms.Button addcustomer;
+        private System.Windows.Forms.Button print;
+        private System.Windows.Forms.Button preview;
+        private System.Windows.Forms.Button close;
+        private System.Drawing.Printing.PrintDocument printDocument;
+        private System.Windows.Forms.PrintPreviewDialog printPreviewDialog;
+    }
+}
+
---
+++ C#/Windows_Form_Application/PDFCreator/MainForm.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PDFCreator
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void close_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void addcustomer_Click(object sender, EventArgs e)
+        {
+            name.Clear();
+            mobno.Clear();
+            productlist.SelectedIndex = 0;
+            quantitycb.SelectedIndex = 0;
+            price.Clear();
+            totalamount.Clear();
+        }
+
+        private void price_TextChanged(object sender, EventArgs e)
+        {
+            if (!price.Text.Equals("") && !quantitycb.Text.Equals(""))
+            {
+                decimal amount = Convert.ToDecimal(price.Text) * Convert.ToInt32(quantitycb.Text);
+                totalamount.Text = amount.ToString();
+            }
+        }
+
+        private void quantitycb_TextChanged(object sender, EventArgs e)
+        {
+            if (!price.Text.Equals("") && !quantitycb.Text.Equals(""))
+            {
+                decimal amount = Convert.ToDecimal(price.Text) * Convert.ToInt32(quantitycb.Text);
+                totalamount.Text = amount.ToString();
+            }
+        }
+
+        private void preview_Click(object sender, EventArgs e)
+        {
+            printPreviewDialog.Document = printDocument;
+            printPreviewDialog.ShowDialog();
+        }
+
+        private void printDocument_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+        {
+
+        }
+    }
+}
