f60adaa7442dbdffd745dff910df373d3c7f0e74
Surajj4837-th <surajj4837@gmail.com>
2024-07-21 12:31:58 +0530

    Created encryption and dycryption functions.

--- C#/Windows_Form_Application/StringEncryption/EncryptionDecryption.cs
+++ C#/Windows_Form_Application/StringEncryption/EncryptionDecryption.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace StringEncryption
 {
@@ -17,6 +18,42 @@ namespace StringEncryption
         // Decrypt edata1 with key k2 using symmetric decryption, creating data2.
         // data2 should equal data1.
 
+        public static byte[] EncryptData(string pwd1, byte[] salt1, string data1, ref Aes encAlg, int myIterations)
+        {
+            Rfc2898DeriveBytes k1 = new Rfc2898DeriveBytes(pwd1, salt1, myIterations);
+            encAlg.Key = k1.GetBytes(16);
+
+            // Encrypt the data.
+            MemoryStream encryptionStream = new MemoryStream();
+            CryptoStream encrypt = new CryptoStream(encryptionStream, encAlg.CreateEncryptor(), CryptoStreamMode.Write);
+            byte[] utfD1 = new System.Text.UTF8Encoding(false).GetBytes(data1);
+
+            encrypt.Write(utfD1, 0, utfD1.Length);
+            encrypt.FlushFinalBlock();
+            encrypt.Close();
+            byte[] edata1 = encryptionStream.ToArray();
+            k1.Reset();
+
+            return edata1;
+        }
+
+        public static string DecryptData(Rfc2898DeriveBytes k2, Aes encAlg, byte[] edata1)
+        {
+            // Try to decrypt, thus showing it can be round-tripped.
+            Aes decAlg = Aes.Create();
+            decAlg.Key = k2.GetBytes(16);
+            decAlg.IV = encAlg.IV;
+            MemoryStream decryptionStreamBacking = new MemoryStream();
+            CryptoStream decrypt = new CryptoStream(decryptionStreamBacking, decAlg.CreateDecryptor(), CryptoStreamMode.Write);
+            decrypt.Write(edata1, 0, edata1.Length);
+            decrypt.Flush();
+            decrypt.Close();
+            k2.Reset();
+            string data2 = new UTF8Encoding(false).GetString(decryptionStreamBacking.ToArray());
+
+            return data2;
+        }
+
         public static void Main(string[] args)
         {
             string password = "Sample";
@@ -29,6 +66,7 @@ namespace StringEncryption
             else
             {
                 string pwd1 = password;
+
                 // Create a byte array to hold the random value.
                 byte[] salt1 = new byte[8];
                 using (RNGCryptoServiceProvider rngCsp = new RNGCryptoServiceProvider())
@@ -39,36 +77,19 @@ namespace StringEncryption
 
                 //data1 can be a string or contents of a file.
                 string data1 = "Some test data";
+
                 //The default iteration count is 1000 so the two methods use the same iteration count.
                 int myIterations = 1000;
+
                 try
                 {
-                    Rfc2898DeriveBytes k1 = new Rfc2898DeriveBytes(pwd1, salt1, myIterations);
                     Rfc2898DeriveBytes k2 = new Rfc2898DeriveBytes(pwd1, salt1);
-                    // Encrypt the data.
+
                     Aes encAlg = Aes.Create();
-                    encAlg.Key = k1.GetBytes(16);
-                    MemoryStream encryptionStream = new MemoryStream();
-                    CryptoStream encrypt = new CryptoStream(encryptionStream, encAlg.CreateEncryptor(), CryptoStreamMode.Write);
-                    byte[] utfD1 = new System.Text.UTF8Encoding(false).GetBytes(data1);
 
-                    encrypt.Write(utfD1, 0, utfD1.Length);
-                    encrypt.FlushFinalBlock();
-                    encrypt.Close();
-                    byte[] edata1 = encryptionStream.ToArray();
-                    k1.Reset();
+                    byte[] edata1 = EncryptData(pwd1, salt1, data1, ref encAlg, myIterations);
 
-                    // Try to decrypt, thus showing it can be round-tripped.
-                    Aes decAlg = Aes.Create();
-                    decAlg.Key = k2.GetBytes(16);
-                    decAlg.IV = encAlg.IV;
-                    MemoryStream decryptionStreamBacking = new MemoryStream();
-                    CryptoStream decrypt = new CryptoStream(decryptionStreamBacking, decAlg.CreateDecryptor(), CryptoStreamMode.Write);
-                    decrypt.Write(edata1, 0, edata1.Length);
-                    decrypt.Flush();
-                    decrypt.Close();
-                    k2.Reset();
-                    string data2 = new UTF8Encoding(false).GetString(decryptionStreamBacking.ToArray());
+                    string data2 = DecryptData(k2, encAlg, edata1);
 
                     if (!data1.Equals(data2))
                     {
@@ -77,8 +98,6 @@ namespace StringEncryption
                     else
                     {
                         Console.WriteLine("The two values are equal.");
-                        Console.WriteLine("k1 iterations: {0}", k1.IterationCount);
-                        Console.WriteLine("k2 iterations: {0}", k2.IterationCount);
                     }
                 }
                 catch (Exception e)
