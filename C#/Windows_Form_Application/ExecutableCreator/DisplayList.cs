64c947f39eb90848bd66297591875b4d1aa7a993
Surajj4837-th <surajj4837@gmail.com>
2024-08-25 20:46:07 +0530

    Added project to create a database project and create its executable. Later modify the code and check the executable is able to modify the existing application.

---
+++ C#/Windows_Form_Application/ExecutableCreator/DisplayList.cs
@@ -0,0 +1,41 @@
+﻿using DatabaseProject;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExecutableCreator
+{
+    public partial class DisplayList : Form
+    {
+        DataTable dt_Customers = new DataTable();
+        DBAccess obj_DBAccess = new DBAccess();
+        public DisplayList()
+        {
+
+            InitializeComponent();
+            dt_Customers.Columns.Add("Name");
+            dt_Customers.Columns.Add("Surname");
+
+            dataGridView1.DataSource = dt_Customers;
+            dataGridView1.Columns[0].HeaderText = "Name";
+            dataGridView1.Columns[0].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            dataGridView1.Columns[1].HeaderText = "Surname";
+            dataGridView1.Columns[1].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+
+            string query = "Select * from Table1";
+
+            dt_Customers.Rows.Clear();
+            obj_DBAccess.readDatathroughAdapter(query, dt_Customers);
+            obj_DBAccess.closeConn();
+
+            dataGridView1.Update();
+            dataGridView1.Refresh();
+        }
+    }
+}
---
+++ C#/Windows_Form_Application/ExecutableCreator/Form1.cs
@@ -0,0 +1,46 @@
+﻿using DatabaseProject;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace ExecutableCreator
+{
+    public partial class Form1 : Form
+    {
+        DBAccess obj_DBAccess = new DBAccess();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(textBox1.Text.Equals("") || textBox2.Text.Equals(""))
+            {
+
+            }
+            else
+            {
+                SqlCommand insertCommand = new SqlCommand("insert into Table1 (Name, Surname) values (@Name, @Surname)");
+                insertCommand.Parameters.AddWithValue("@Name", textBox1.Text.ToString());
+                insertCommand.Parameters.AddWithValue("@Surname", textBox2.Text.ToString());
+
+                obj_DBAccess.executeQuery(insertCommand);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form Disp = new DisplayList();
+            Disp.Show();
+        }
+    }
+}
